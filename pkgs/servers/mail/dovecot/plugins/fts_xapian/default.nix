cb159dd588518aaa59cfbea977ef2a41872af998
Guillaume Girol <symphorien+git@xlumurb.eu>
2021-11-10 12:00:00 +0000

    dovecot_fts_xapian: 1.4.14 -> 1.5.0
    and align name with what repology expects

--- pkgs/servers/mail/dovecot/plugins/fts_xapian/default.nix
+++ pkgs/servers/mail/dovecot/plugins/fts_xapian/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, autoconf, automake, pkg-config, dovecot, libtool, xapian, icu64 }:
 stdenv.mkDerivation rec {
-  pname = "fts-xapian";
-  version = "1.4.14";
+  pname = "dovecot-fts-xapian";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner = "grosjo";
     repo = "fts-xapian";
     rev = version;
-    sha256 = "sha256-Banyg10AiM1Jw6Zfl4Dcpc0/6Km48lLVuQ3xRLylE7k=";
+    sha256 = "1d4l38bpyq14xv8brsn9xwz7kq0vgbcnak8w2q3m52lk6i0jxgz6";
   };
 
   buildInputs = [ dovecot xapian icu64 ];
